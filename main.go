fe94ce5cefeacb7d8f9a8f83277b2cb94ebf967a
Sohbetbackend <sgurbanov888@gmail.com>
2023-08-29 15:30:53 +0500

    fix little errors

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"github.com/Sohbetbackend/golang-react/database"
 	"github.com/Sohbetbackend/golang-react/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/middleware/cors"
 )
 
 func main() {
 	database.Connect()
+
     app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
-    app.Listen(":3000")
+    app.Listen(":8000")
 }
