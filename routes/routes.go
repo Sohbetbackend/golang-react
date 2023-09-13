21c3ad7bc2de7e1a82992c1f3cd065dec25f74b9
Sohbetbackend <sgurbanov888@gmail.com>
2023-09-13 16:08:33 +0500

    logout part

--- routes/routes.go
+++ routes/routes.go
@@ -9,5 +9,6 @@ func Setup(app *fiber.App) {
 	
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
-	app.Post("/api/user", controllers.User)
+	app.Get("/api/user", controllers.User)
+	app.Get("/api/logout", controllers.Logout)
 }
